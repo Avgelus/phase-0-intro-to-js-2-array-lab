bea3b3a892feb2859167dbb82ec5c1a37c44422a
+ <+>
2023-08-17 13:28:48 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo");
+}
+
+function appendCat(name) {
+    const newCat = [...cats, "Broom"];
+    return newCat
+}
+
+
+function prependCat(name) {
+    const newCat = ["Arnold", ...cats];
+    return newCat
+}
+
+function removeLastCat() {
+    const newCat = cats.slice(0, -1);
+    return newCat
+}
+
+function removeFirstCat() {
+    const newCat = cats.slice(1);
+    return newCat
+}
+
